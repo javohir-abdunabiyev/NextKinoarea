8fabe693e03a1e465a25825640fecb841a303ac9
javohir-abdunabiyev <jahaajahaaa2@gmail.com>
2025-04-13 23:58:13 +0500

    added

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
     domains: ["image.tmdb.org"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.css$/i,
+        include: /[\\/]src[\\/]components[\\/]custom[\\/]/,
+        use: ['ignore-loader']
+      });
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
